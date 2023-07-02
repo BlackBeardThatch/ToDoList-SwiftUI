dc027734a9f276060c2c6ee184974fa86a0914ec
Jason Hung <cp3jason@gmail.com>
2023-07-02 23:43:05 +0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Jason Hung on 2023/7/2.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
